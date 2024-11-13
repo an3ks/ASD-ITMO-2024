c42222c6abab9c75ac04e739fcafee170fc3a90f
an3ks <avdienkodanya99@gmail.com>
2024-11-13 18:14:28 +0300

    test fix

--- lab3/task4/test/test_ex4.py
+++ lab3/task4/test/test_ex4.py
@@ -8,8 +8,9 @@ from lab3.utils.utils import time_memory_tracking
 
 class TestLotteryFunction(unittest.TestCase):
 
-    def test_basic_case(self):
+    def test_should_lottery1(self):
         """базовый"""
+        # given
         data = [
             (3, 3),
             (1, 5),
@@ -18,11 +19,16 @@ class TestLotteryFunction(unittest.TestCase):
             [2, 4, 6]
         ]
         expected_result = [2, 3, 2]
+
+        # when
         result = lottery(data)
+
+        # then
         self.assertEqual(result, expected_result)
 
-    def test_no_intersection(self):
+    def test_should_lottery2(self):
         """отрезки не пересекаются с точками"""
+        # given
         data = [
             (2, 3),
             (1, 2),
@@ -30,22 +36,32 @@ class TestLotteryFunction(unittest.TestCase):
             [3, 4, 7]
         ]
         expected_result = [0, 0, 0]
+
+        # when
         result = lottery(data)
+
+        # then
         self.assertEqual(result, expected_result)
 
-    def test_all_points_inside_segment(self):
+    def test_should_lottery3(self):
         """все точки внутри одного отрезка"""
+        # given
         data = [
             (1, 3),
             (0, 10),
             [1, 5, 9]
         ]
         expected_result = [1, 1, 1]
+
+        # when
         result = lottery(data)
+
+        # then
         self.assertEqual(result, expected_result)
 
-    def test_boundary_case(self):
+    def test_should_lottery4(self):
         """точки находятся на границах отрезков"""
+        # given
         data = [
             (2, 3),
             (1, 3),
@@ -53,11 +69,16 @@ class TestLotteryFunction(unittest.TestCase):
             [3, 4, 5]
         ]
         expected_result = [2, 1, 1]
+
+        # when
         result = lottery(data)
+
+        # then
         self.assertEqual(result, expected_result)
 
-    def test_large_input(self):
+    def test_should_lottery5(self):
         """случай с большими входными данными для проверки производительности"""
+        # given
         s, p = 100000, 100000
         segments = [(i, i + 10) for i in range(0, s * 10, 10)]
         points = [i for i in range(5, s * 10, 10)]
@@ -65,7 +86,11 @@ class TestLotteryFunction(unittest.TestCase):
         expected_result = [1] * p
         data = [(s, p)] + segments + [points]
         start_time = time.perf_counter()
+
+        # when
         result = lottery(data)
+
+        # then
         self.assertEqual(result, expected_result)
         time_memory_tracking(start_time)
 
