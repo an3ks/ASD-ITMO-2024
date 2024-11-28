6017e58ed7c7447dcf51779c175870580cf3a041
an3ks <avdienkodanya99@gmail.com>
2024-11-28 12:59:51 +0300

    lab structure

---
+++ lab4/task6/test/test_ex6.py
@@ -0,0 +1,38 @@
+import unittest
+from random import randint
+from ..src.ex1 import quick_sort_upgrade
+
+
+class TestQuickSort(unittest.TestCase):
+
+    def test_should_quick_sort1(self):
+        """Большой массив одинаковых чисел, кроме одного"""
+        # given
+        arr = [2] + [1] * 10 ** 5
+        # then
+        self.assertEqual(quick_sort_upgrade(arr), sorted(arr))
+
+    def test_should_quick_sort2(self):
+        """Большой массив рандомных чисел"""
+        # given
+        arr = [randint(0, 10 ** 9) for i in range(10 ** 5)]
+        # then
+        self.assertEqual(quick_sort_upgrade(arr), sorted(arr))
+
+    def test_should_quick_sort3(self):
+        """Массив из одного элемента"""
+        # given
+        arr = [1]
+        self.assertEqual(quick_sort_upgrade(arr), [1])
+
+
+    def test_should_quick_sort4(self):
+        """Пустой массив"""
+        # given
+        arr = []
+        # then
+        self.assertEqual(quick_sort_upgrade(arr), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
